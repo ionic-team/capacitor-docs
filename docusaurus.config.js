738042ca609873eb886ed986ff8862bd9d41037c
IT-MikeS <20338451+IT-MikeS@users.noreply.github.com>
2022-08-23 12:22:58 -0400

    chore: resolve deps issues

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -187,7 +187,7 @@ module.exports = {
     },
   },
   plugins: [
-    'docusaurus-plugin-sass',
+    // 'docusaurus-plugin-sass',
     [
       'docusaurus-plugin-module-alias',
       {
@@ -253,6 +253,47 @@ module.exports = {
         },
       };
     },
+    function(_, {id, ...options}) {
+      return {
+        name: 'docusaurus-plugin-sass-builtin',
+        configureWebpack(_, isServer, utils) {
+          const { getStyleLoaders } = utils;
+          const isProd = process.env.NODE_ENV === 'production';
+          return {
+            module: {
+              rules: [{
+                test: /\.s[ca]ss$/,
+                oneOf: [{
+                  test: /\.module\.s[ca]ss$/,
+                  use: [
+                    ...getStyleLoaders(isServer, {
+                      modules: {
+                        localIdentName: isProd
+                          ? `[local]_[hash:base64:4]`
+                          : `[local]_[path][name]`,
+                        exportOnlyLocals: isServer,
+                      },
+                      importLoaders: 2,
+                      sourceMap: !isProd,
+                    }), {
+                      loader: 'sass-loader',
+                      options: options || {}
+                    }
+                  ]
+                }, {
+                  use: [
+                    ...getStyleLoaders(isServer), {
+                      loader: 'sass-loader',
+                      options: options || {}
+                    }
+                  ]
+                }]
+              }]
+            }
+          };
+        }
+      };
+    }
   ],
   themes: [
     [
