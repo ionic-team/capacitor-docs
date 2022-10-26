2a882a630504939d3fcb21535854ec3a9735b406
jcesarmobile <jcesarmobile@gmail.com>
2022-10-26 17:32:14 +0200

    fix: correct edit this page link on plugin pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -209,7 +209,7 @@ module.exports = {
             return 'https://crowdin.com/project/capacitor-docs';
           }
           if ((match = docPath.match(/api\/(.*)\.md/)) != null) {
-            return `https://github.com/ionic-team/capacitor-docs/tree/main/docs/api/${match[1]}.md`;
+            return `https://github.com/ionic-team/capacitor-plugins/tree/main/${match[1]}/README.md`;
           }
           if ((match = docPath.match(/cli\/commands\/(.*)\.md/)) != null) {
             return `https://github.com/ionic-team/capacitor-docs/tree/main/docs/cli/commands/${match[1].replace(
