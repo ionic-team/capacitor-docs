24be08afb8bfb826d002eb0e94ad5cee76ed8dcc
jared <jaredcbaum@gmail.com>
2023-11-02 11:12:09 -0600

    refactor(config): remove built in sass plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -207,52 +207,6 @@ module.exports = {
         },
       };
     },
-    function (_, { id, ...options }) {
-      return {
-        name: 'docusaurus-plugin-sass-builtin',
-        configureWebpack(_, isServer, utils) {
-          const { getStyleLoaders } = utils;
-          const isProd = process.env.NODE_ENV === 'production';
-          return {
-            module: {
-              rules: [
-                {
-                  test: /\.s[ca]ss$/,
-                  oneOf: [
-                    {
-                      test: /\.module\.s[ca]ss$/,
-                      use: [
-                        ...getStyleLoaders(isServer, {
-                          modules: {
-                            localIdentName: isProd ? `[local]_[hash:base64:4]` : `[local]_[path][name]`,
-                            exportOnlyLocals: isServer,
-                          },
-                          importLoaders: 2,
-                          sourceMap: !isProd,
-                        }),
-                        {
-                          loader: 'sass-loader',
-                          options: options || {},
-                        },
-                      ],
-                    },
-                    {
-                      use: [
-                        ...getStyleLoaders(isServer),
-                        {
-                          loader: 'sass-loader',
-                          options: options || {},
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            },
-          };
-        },
-      };
-    },
   ],
   presets: [
     [
