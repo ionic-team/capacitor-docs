f107679268960b6e3d838a0345a2d261261a3126
jaredcbaum <61951482+jaredcbaum@users.noreply.github.com>
2024-06-05 08:48:46 -0600

    feat(): os announcement bar (#317)
    * chore(deps): bump ionic docusaurus theme
    
    * chore(config): add announcement bar
    
    * styles(announcement bar): styles os announcement bar
    
    * fix(announcement bar): better contrast
    
    * chore(deps): bump docusaurus related deps to latest

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,6 +29,12 @@ module.exports = {
   organizationName: 'ionic-team',
   projectName: 'capacitor-docs',
   themeConfig: {
+    announcementBar: {
+      id: 'announcement-bar',
+      content:
+        '<a href="https://www.outsystems.com/?utm_source=ionic&utm_medium=referral&utm_campaign=ionic-referral&utm_term=none&utm_content=other&utm_campaignteam=digital-mktg&utm_partner=none" target="_blank" rel="noopener"><span>An <strong>OutSystems</strong> Company →</span></a>',
+      isCloseable: false,
+    },
     colorMode: {
       defaultMode: 'light',
     },
